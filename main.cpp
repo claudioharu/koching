ffcd0d4f27620f4640fae39f0f5c4537fe11c02f
claudioharu <claudioharu@gmail.com>
2014-11-26 19:24:26 -0200

    comments

--- main.cpp
+++ main.cpp
@@ -23,11 +23,12 @@ int main(int argc, char* argv[])
 	
 	short flag = atoi(argv[1]);
 	Mat dictionary;
-	//Construct BOWKMeansTrainer
-	//the number of bags
+
 	int dictionarySize = 180;
 
 	char buf[255];
+
+	// reading trainning paths	
 	ifstream ifs("trainpaths.txt");
 	
 	string trainPaths[22];
@@ -41,15 +42,14 @@ int main(int argc, char* argv[])
 		if (line.compare("") != 0)
 		{
 			line = line + "/";
-			// istringstream iss(line);
 			trainPaths[j] = line;
 			j++;
-			cout << line << endl;
 		}
 
 	}
 	ifs.close();
 
+	// reading test paths	
 	ifs.open("testpaths.txt");
 	j = 0;
 	while(!ifs.eof())
@@ -58,21 +58,19 @@ int main(int argc, char* argv[])
 		string line(buf);
 		if (line.compare("") != 0)
 		{
-			// line = line + "/";
-			// istringstream iss(line);
+
 			testPaths[j] = line;
 			j++;
-			cout << line << endl;
+			// cout << line << endl;
 		}
 
 	}
 	ifs.close();
 	
-
-
+// building the dictionary 
 if (flag == 1)
 {
-	cout << argv[1] << "\n";
+	// cout << argv[1] << "\n";
 
 	string dir = "train/", filepath, imgpath;
 	DIR *dp;
@@ -96,92 +94,86 @@ if (flag == 1)
 
 	unsigned char isFile =0x8;
 
-	cout << "------- build vocabulary ---------\n";
+	cout << "------- building vocabulary ---------\n";
 
-	cout << "extract descriptors.."<<endl;
+	cout << "extracting descriptors.."<<endl;
 
-	// while (dirp = readdir( dp ))
-	// {
 	for(int i = 0; i < 21; i++)
 	{
 		filepath = trainPaths[i];
-		// filepath = dir + dirp->d_name;
-		cout << filepath << endl;
 
 		dp1 = opendir( filepath.c_str() );
 
 		while (dirp1 = readdir( dp1 ))
 		{
 
-			// filepath = dir + dirp->d_name;
-			cout << filepath << endl;
-
 			imgpath = filepath+dirp1->d_name;
+
+			// checking files
 			if (dirp1->d_type != isFile) continue;
 			
-			cout << imgpath << endl;
-
 			img = imread(imgpath);
+			// detect feature points
 			detector.detect(img, keypoints);
+			// compute the descriptors for each keypoint
 			extractor->compute(img, keypoints, descriptors);
-			
+			// put the all feature descriptors in a single Mat object 
 			training_descriptors.push_back(descriptors);
 
 		}
 		closedir(dp1);
 	}
 
-	cout << endl;
 	closedir( dp );
 
 	cout << "Total descriptors: " << training_descriptors.rows << endl;
-
-	BOWKMeansTrainer bowtrainer(180); //num clusters
+	
+	// create the BoW trainer
+	BOWKMeansTrainer bowtrainer(180); // num clusters
 	bowtrainer.add(training_descriptors);
 	cout << "cluster BOW features" << endl;
+
+	// cluster the feature vectors
 	dictionary = bowtrainer.cluster();
 	
 	FileStorage fs("dictionary.yml", FileStorage::WRITE);
+	// store the vocabulary
 	fs << "vocabulary" << dictionary;
 	fs.release();
 }
+
+// training
 else
 {
 	string dir = "train/", filepath, imgpath;
 	DIR *dp, *dp1;
 	struct dirent *dirp, *dirp1;
-	// dp = opendir( dir.c_str() );
 
-
-    //prepare BOW descriptor extractor from the dictionary    
+    // prepare BOW descriptor extractor from the dictionary    
     Mat dictionary; 
     FileStorage fs("dictionary.yml", FileStorage::READ);
     fs["vocabulary"] >> dictionary;
     fs.release();    
     
-    //create a nearest neighbor matcher
+    // create a nearest neighbor matcher
     Ptr<DescriptorMatcher> matcher(new FlannBasedMatcher);
-    //create Sift feature point extracter
+
+    // create Surf feature point extracter
   	SurfFeatureDetector detector(1000);
-    //create Sift descriptor extractor
-	Ptr<DescriptorExtractor > extractor(new SurfDescriptorExtractor());//  extractor;
 
+    // create Surf descriptor extractor
+	Ptr<DescriptorExtractor > extractor(new SurfDescriptorExtractor());
 
-    //To store the keypoints that will be extracted by SIFT
+    // keypoints that will be extracted by Surf
     vector<KeyPoint> keypoints;  
 
-    //create BoF (or BoW) descriptor extractor
+    // create BoF descriptor extractor
     BOWImgDescriptorExtractor bowide(extractor,matcher);
-    //Set the dictionary with the vocabulary we created in the first step
+
+    // set the dictionary with the vocabulary we created in the first step
     bowide.setVocabulary(dictionary);
  
-    //To store the image file name
-    char * filename = new char[100];
-    //To store the image tag name - only for save the descriptor in a file
-    char * imageTag = new char[10];
- 
-    //open the file to write the resultant descriptor
-    // FileStorage fs1("descriptor.yml", FileStorage::WRITE);
+	cout << endl <<"Extracting histograms in the form of BOW for each image "<< endl;
 
     Mat bowDescriptor; 
     Mat img;
@@ -189,31 +181,27 @@ else
 	Mat trainingData(0, dictionarySize, CV_32FC1);
 
 	int count = 0;
- //    while (dirp = readdir( dp ))
-	// {
+
 	for(int i = 0; i < 21; i++){
 		filepath = trainPaths[i];
-		// filepath = dir + dirp->d_name;
-		// cout << filepath << endl;
-
 		dp1 = opendir( filepath.c_str() );
 
 		while (dirp1 = readdir( dp1 ))
 		{
+			// checking files
 			if (dirp1->d_type != isFile) continue;
 
-			// filepath = dir + dirp->d_name;
-			cout << filepath << endl;
 			imgpath = filepath  +dirp1->d_name;
-			cout << "img: " << imgpath << endl;
-
 			img = imread(imgpath);
-			if(img.data )                              // Check for invalid input
+
+			// check for invalid input
+			if(img.data )       
 			{
-				// cout << "label: " << dirp->d_name << endl;
+				// detect feature points
 			    detector.detect(img, keypoints);
 				bowide.compute(img, keypoints, bowDescriptor);
 				if(!bowDescriptor.empty() ){
+					// adding labels 
 					labels.push_back((float)i);
 					trainingData.push_back(bowDescriptor);
 				}
@@ -225,20 +213,13 @@ else
 		closedir(dp1);
 		
 	}  
-
-	// closedir( dp );
-
-    
-	cout << "train data: " <<  bowDescriptor.rows << " " << bowDescriptor.cols << endl;
-	cout << "train data: " <<  trainingData.rows << " " << trainingData.cols << endl;
-	cout << "labels data: " << labels.rows << " " << labels.cols << endl;
 	
- 	//Setting up SVM parameters
+ 	// setting up SVM parameters
 	CvSVMParams params;
 	params.kernel_type = CvSVM::RBF;
 	params.svm_type = CvSVM::C_SVC;
-	params.gamma = 0.40425000000000009;
-	params.C = 299.50000000000000;
+	params.gamma = 0.42545000000000009;
+	params.C = 301.30000000000000;
 	params.term_crit = cvTermCriteria(CV_TERMCRIT_ITER,100,0.000001);
 	CvSVM svm;
 
@@ -247,7 +228,8 @@ else
 
 	bool res = svm.train(trainingData, labels, cv::Mat(), cv::Mat(), params);
 
-	cout << "Processing evaluation data..." << endl;
+	cout << endl << "Processing evaluation data..." << endl;
+	cout << endl;
 
 	Mat groundTruth(0, 1, CV_32FC1);
 	Mat evalData(0, dictionarySize, CV_32FC1);
@@ -259,56 +241,44 @@ else
 
 	dir = "test/";
 
-	// dp = opendir( dir.c_str() );
- //    while (dirp = readdir( dp ))
-	// {
-	for(int i = 0; i < 21; i++){
+	for(int i = 20; i < 21; i++){
 		filepath = testPaths[i];
-		// filepath = dir + dirp->d_name;
-		// cout << filepath << endl;
-
 		dp1 = opendir( filepath.c_str() );
 
 		while (dirp1 = readdir( dp1 ))
 		{
+			// checking files
 			if (dirp1->d_type != isFile) continue;
 
-			// filepath = testPaths[i] + dirp->d_name;
-			cout << filepath << endl;
 			imgpath = filepath + "/" + dirp1->d_name;
-			cout << "img: " << imgpath << endl;
-
 			img2 = imread(imgpath);
-			if(img2.data )                              // Check for invalid input
+
+			// check for invalid input
+			if(img2.data )         
 			{
-				// cout << "label: " << dirp->d_name << endl;
+				// detect feature points
 			    detector.detect(img2, keypoints2);
 				bowide.compute(img2, keypoints2, bowDescriptor2);
 				if(!bowDescriptor2.empty()){
-					cout << "galo" << endl;
 
 					evalData.push_back(bowDescriptor2);
+					// right answer
 					groundTruth.push_back((float) i);
 					float response = svm.predict(bowDescriptor2);
+					// predicted results
 					results.push_back(response);
 				}
 			}
 			    
 
 		}
-
 		closedir(dp1);
 	} 
 
-	// closedir( dp );
-
-	cout << " evalData.rows: " <<  evalData.rows << endl;
-
-	cout << "(double) countNonZero(groundTruth - results): " << (double) countNonZero(groundTruth - results) << endl;
-
+	// calculate the number of unmatched classes 
 	double errorRate = (double) countNonZero(groundTruth - results) / evalData.rows;
 	printf("%s%f","Error rate is ",errorRate);
-	cout << endl;
+	cout << endl << endl;
 
 }
 
